b31ec9b44519089a794b50279684a19971df0bcd
Eka Putra <eka@insight.co>
2020-10-20 23:51:57 +1100

    Added script to convert jsonlines to csv

---
+++ 2_cleaner/jsonline_to_csv.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import argparse
+
+
+def convert(in_file, out_file):
+    df = pd.read_json(in_file, lines=True)
+    df = df[['type', 'message', 'sender']]
+    df['message'] = df['message'].apply(
+        lambda m: m.strip().lower().replace('\n', ' '))
+    # remove duplicates
+    df.drop_duplicates(subset=['type', 'message'], inplace=True)
+
+    df.to_csv(out_file, index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dataset cleaner')
+    parser.add_argument(
+        '--jl-in',
+        required=True,
+        help='Input file to cleanup (must be Jsonline from scrapper)'
+    )
+    parser.add_argument(
+        '--csv-out',
+        required=True,
+        help='CSV Output file name'
+    )
+    args = parser.parse_args()
+
+    input_file = args.jl_in
+    output_file = args.csv_out
+
+    convert(input_file, output_file)
